bb10eb54094a8f226678c1a29b98b0b93483c46b
Josh Dunne <joshua.m.dunne@gmail.com>
2024-03-14 17:20:22 -0700

    DB and Auth 06: Add Login and login/logout states

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { useScrollTop } from "@/hooks/use-scroll-top";
-import { cn } from "@/lib/utils";
-import { Logo } from "./logo";
-import { ModeToggle } from "@/components/mode-toggle";
 import { useConvexAuth } from "convex/react";
-import { SignInButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import Link from "next/link";
+
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/spinner";
+import { cn } from "@/lib/utils";
+
+import { Logo } from "./logo";
 
 export const Navbar = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -16,7 +19,7 @@ export const Navbar = () => {
   return (
     <div
       className={cn(
-        "z-50 bg-background dark:bg-[#1f1f1f] fixed top-0 flex items-center w-full p-6",
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
         scrolled && "border-b shadow-sm"
       )}
     >
@@ -35,10 +38,16 @@ export const Navbar = () => {
             </SignInButton>
           </>
         )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
   );
 };
-
-export default Navbar;
--- components/spinner.tsx
+++ components/spinner.tsx
@@ -9,8 +9,8 @@ const spinnerVariants = cva("text-muted-foreground animate-spin", {
     size: {
       default: "w-4 h-4",
       sm: "w-2 h-2",
-      md: "w-6 h-6",
-      lg: "w-10 h-10",
+      lg: "w-6 h-6",
+      icon: "w-10 h-10",
     },
   },
   defaultVariants: {
